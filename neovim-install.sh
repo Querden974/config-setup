e37ea322a55f5a35451b3648faaeaa30d9f36b37
Querden <100098395+Querden974@users.noreply.github.com>
2026-02-05 20:48:39 +0100

    Add Neovim installation script with Lazyvim setup
    Install Neovim and set up Lazyvim configuration.

---
+++ neovim-install.sh
@@ -0,0 +1,13 @@
+cd /tmp
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
+tar xzf nvim-linux-x86_64.tar.gz
+sudo mv nvim-linux-x86_64 /opt/nvim
+sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
+source ./bashrc
+rm /tmp/nvim-linux-x86_64.tar.gz
+
+echo "Install Lazyvim"
+touch ~/.config/nvim/init.lua
+cd /tmp
+wget https://github.com/Querden974/config-setup/blob/main/lazyvim_single.lua
+cp lazyvim_single.lua ~/.config/nvim/init.lua

3e053acf62881db9f5ab547d8d015a884fa85384
Querden <100098395+Querden974@users.noreply.github.com>
2026-02-05 21:25:15 +0100

    Add installation steps for Lazyvim configuration

--- lazvim-install.sh
+++ lazvim-install.sh
@@ -1,4 +1,5 @@
 echo "Install Lazyvim"
+mkdir -p ~/.config/nvim/
 touch ~/.config/nvim/init.lua
 cd /tmp
 wget https://raw.githubusercontent.com/Querden974/config-setup/refs/heads/main/lazyvim_single.lua
